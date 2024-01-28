5bba83c508edef73b2cf2b2d545977e100362c2f
LalitVerma7 <157007425+LalitVerma7@users.noreply.github.com>
2024-01-28 18:58:24 +0530

    Add files via upload

---
+++ Basics/SimpleIntrest.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+
+public class SimpleIntrest {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Enter the Principal: ");
+        float prin = sc.nextFloat();
+
+        System.out.print("Enter the Rate: ");
+        float rate = sc.nextFloat();
+
+        System.out.print("Enter the Time(in years): ");
+        float time = sc.nextFloat();
+
+        float si = prin*rate*time/100;
+
+        System.out.println("Simple Intrest: " + si);
+    }
+}
